e0f33264e15a538c6346330d2e4031f815a6265b
vikaswadile <100255449+vikaswadile@users.noreply.github.com>
2023-04-14 13:19:41 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#this shell script is to clone repo update the file & push the changes made
+#author : Vikas 
+#written on : 14th-Apr-2023
+
+# check if the repository is already present
+if [ -d "git_jenkins" ]; then
+  cd git_jenkins
+  # pull the latest changes from the remote branch
+
+echo "Repository already exist locally, pulling latest changes from remote repository"
+
+  git pull origin main
+
+else
+  # clone the repository
+
+echo "Cloning remote repository to local !"
+
+sleep 2
+
+  git clone git@github.com:vikaswadile/git_jenkins.git
+  cd git_jenkins
+fi
+
+#listing files present in the directory
+sleep 2
+echo "=================================================="
+echo "Below are the files present in repository"
+echo "=================================================="
+
+ls -1p | grep -v / | awk '{print $1}'
+
+# prompt the user for the file to update
+sleep 2
+echo "=================================================="
+read -p "Enter the name of the file you want to update: " file_name
+echo "=================================================="
+sleep 2
+
+# edit the file
+
+echo "==================================================="
+echo "you have selected $file_name,please wait opening vi editor for you."
+echo "==================================================="
+sleep 2
+
+vi $file_name
+
+echo "===================================================="
+echo "file $file_name is updated & saved succesfully."
+echo "==================================================="
+sleep 2
+
+# setting golabl config
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+
+# commit the changes
+echo "==================================================="
+echo "performing git add step for file $file_name"
+echo "==================================================="
+sleep 2
+
+git add $file_name
+
+echo "==================================================="
+echo "commting the changes you made"
+echo "==================================================="
+
+sleep 2
+
+read -p "write the suitable comment to commit the file : " comment
+
+git commit -m "$comment"
+
+# push the changes to the remote branch
+sleep 2
+echo "==================================================="
+echo "Pushing changes to the remote repository"
+echo "==================================================="
+
+git push origin main
+
+
+
+echo "==================================================="
+echo "Your changes pushed succesfully to the remote repo"
+echo "==================================================="
+
+
